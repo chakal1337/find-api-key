de1bd1c81c21dab6f76a3441de97f6fcccf0e1d8
chakal <54026816+chakal1337@users.noreply.github.com>
2022-11-25 21:29:14 -0500

    Create find.sh

---
+++ find.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+assetfinder -subs-only example.com | sort -u | uniq | httprobe -c 100 | tee probed.txt;
+mkdir alljs;
+cd alljs;
+cat ../probed.txt | xargs -L 1 -P 5 wget -A js -r;
+grep -rEo '.{0,100}API.{0,100}' *;
