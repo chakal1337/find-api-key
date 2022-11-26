9ce7874bb50e9d4db485d7ea94c8e51b73aa1fa0
chakal <54026816+chakal1337@users.noreply.github.com>
2022-11-25 21:29:29 -0500

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-assetfinder -subs-only example.com | sort -u | uniq | httprobe -c 100 | tee probed.txt;
+assetfinder -subs-only $1 | sort -u | uniq | httprobe -c 100 | tee probed.txt;
 mkdir alljs;
 cd alljs;
 cat ../probed.txt | xargs -L 1 -P 5 wget -A js -r;
